4bdcc1225588d95c1e73e88b7df1e3ee9551134b
NHHieuu <hieuhong08122003@gmail.com>
2023-09-07 18:08:24 +0700

    khai bao, gan, in ra man hinh

---
+++ newApp/Models/Hieuclass.cs
@@ -0,0 +1,8 @@
+namespace Your_Namespace
+{
+     public class Hieuclass
+     {
+          public string Name { get; set;}
+          
+     }
+}
--- newApp/Program.cs
+++ newApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lop phat trien phan mem quanly");
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+internal class Program {
+    public static void Main(string[] args)
+{
+    Console.WriteLine("Khai bao va gan gia tri:");
+String hoten = "Nguyen Hong Hieu";
+int songuyen = 20;
+
+Console.WriteLine("Gan gia tri: {0}-{1}",hoten,songuyen);
+}
+}
